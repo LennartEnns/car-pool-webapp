2e9254d682cab99b978e195968138edaae2882cb
Lennart Enns <lennart.enns@sap.com>
2025-01-07 11:49:09 +0100

    Do some setup

--- app.vue
+++ app.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <v-app>
+      <NuxtPage />
+    </v-app>
+  </NuxtLayout>
 </template>
+
+<script setup>
+  const title = 'Car Pool Cost Distributor';
+  const description = 'Fair and flexible distribution of costs for your car pool!';
+
+  useSeoMeta({
+    title,
+    ogTitle: title,
+    description,
+    ogDescription: description,
+    ogLocale: 'en_US',
+  });
+</script>
+
+<style>
+  html {
+    scroll-behavior: smooth;
+  }
+</style>

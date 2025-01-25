a5712169cb36751bfb9ac3fea2fd445e9b9223a9
Lennart Enns <lennart.enns@sap.com>
2025-01-25 22:13:13 +0100

    Change dev DB path

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -2,7 +2,7 @@ export default {
     development: {
       client: 'sqlite3',
       connection: {
-        filename: './server/db/dev/dev.sqlite3',
+        filename: './server/db/dev.sqlite3',
       },
       useNullAsDefault: true,
       seeds: {
